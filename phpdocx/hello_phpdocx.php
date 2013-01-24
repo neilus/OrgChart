34d8d116d596bb380e16a9d687c00cd7d67d9b87
Norbert Sana <sananorbert@gmail.com>
2013-01-24 16:56:43 -0500

    pdf-et sikerült generálni docx-ből, de a formázott szöveget nem sikerült belepakolnom a docx-be,
    még hátra van a template docx-ek próbálgatása, hátha az ottani formázás megmarad a generált docx-ben és bele kerül majd a pdf-be

--- phpdocx/hello_phpdocx.php
+++ phpdocx/hello_phpdocx.php
@@ -1,13 +1,15 @@
-<!DOCTYPE HTML>
-<html><head><title>hello phpdocx library!</title></head>
-<body>
 <?php
 require_once('includes.inc');
 require_once($PHPDOCX.'CreateDocx.inc');
+$output='hello_world';
 $docx = new CreateDocx();
 $docx->addText('Hello world!');
 $docx->createDocx($DOCX_OUT.'hello_world');
 //print $DOCX_OUT.'hello_world.docx';
-print '<a href="'.$DOCX_OUT.'hello_world.docx" >hello phpdocx!</a> ';
+//print '<a href="'.$DOCX_OUT.$output.'.docx" >hello phpdocx!</a> ';
+
+$pdf = new TransformDoc();
+$pdf->setStrFile($DOCX_OUT.$output.'.docx');
+$pdf->generatePDF();
+
 ?>
-</body></html>
--- phpdocx/phpdocx.php
+++ phpdocx/phpdocx.php
@@ -1,13 +1,12 @@
-<!DOCTYPE HTML>
-<html><head><title>hello phpdocx library!</title></head>
-<body>
 <?php
 
 $output='phpdocx_output';
 require_once('includes.inc');
 require_once($PHPDOCX.'CreateDocx.inc');
+require_once($PHPDOCX.'TransformDoc.inc');
+
 $docx = new CreateDocx();
-// adding my formatted text into a string
+$text = array();
 $text[] =
 	array(
 	'text' => 'I am going to write',
@@ -22,10 +21,15 @@ $text[] =
 	array(
 	'text' => ' using bold characters.',
 	);
-// putting the my string in to the docx file
 $docx->addText($text);
+
 $docx->createDocx($DOCX_OUT.$output);
 
-print '<a href="'.$DOCX_OUT.$output.'.docx" >GRAB THE docx file!</a> ';
+//print '<a href="'.$DOCX_OUT.$output.'.docx" >GRAB THE docx file!</a> ';
+
+$pdf = new TransformDoc();
+//$pdf->setStrFile($DOCX_OUT .$output .'.docx.');
+//$pdf->setStrFile('../docx/hello_world.docx');
+//$pdf->setStrFile('../docx/hello_bold_text.docx');
+$pdf->generatePDF();
 ?>
-</body></html>
