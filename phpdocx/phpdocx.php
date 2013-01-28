5f7cbaa10b8ecf52381f37b2b1e9d668cdf29eb4
Norbert Sana <sananorbert@gmail.com>
2013-01-28 00:15:03 -0500

    miafasz...

--- phpdocx/phpdocx.php
+++ phpdocx/phpdocx.php
@@ -1,35 +1,18 @@
 <?php
 
-$output='phpdocx_output';
+$output='phpdocx_template_output';
 require_once('includes.inc');
 require_once($PHPDOCX.'CreateDocx.inc');
 require_once($PHPDOCX.'TransformDoc.inc');
 
 $docx = new CreateDocx();
-$text = array();
-$text[] =
-	array(
-	'text' => 'I am going to write',
-	);
-$text[] =
-	array(
-	'text' => ' Hello World!',
-	'b' => 'single',
-	);
 
-$text[] =
-	array(
-	'text' => ' using bold characters.',
-	);
-$docx->addText($text);
-
-$docx->createDocx($DOCX_OUT.$output);
-
-//print '<a href="'.$DOCX_OUT.$output.'.docx" >GRAB THE docx file!</a> ';
+$docx->addTemplate($indoc.'simple_formatted_text.docx');
+$docx->addTemplateVariable('NAME', 'John Smith');
+$docx->createDocx($outdoc.$output);
 
 $pdf = new TransformDoc();
-//$pdf->setStrFile($DOCX_OUT .$output .'.docx.');
-//$pdf->setStrFile('../docx/hello_world.docx');
-//$pdf->setStrFile('../docx/hello_bold_text.docx');
+//$pdf->setStrFile($DOCX_OUT.$output.'.docx');
+$pdf->setStrFile($indoc.'simple_formatted_text.docx');
 $pdf->generatePDF();
 ?>
